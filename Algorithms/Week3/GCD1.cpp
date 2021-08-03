63e86cf86b864da3e2335acb8a15d21d75db3091
Diego Andrés Barbosa Trujillo <da.barbosa10@uniandes.edu.co>
2021-08-03 18:19:31 -0500

    Add files via upload

---
+++ Algorithms/Week3/GCD1.cpp
@@ -0,0 +1,33 @@
+/*
+ * GCD.cpp
+ *
+ *  Created on: 3 ago. 2021
+ *      Author: diegobarbosa
+ */
+#include <iostream>
+using namespace std;
+
+long long gcd(long long a, long long b);
+
+int main(){
+	long long m,n;
+	cin>>n;
+	cin>>m;
+	//cout<< n << m<<endl;
+	long result=gcd(n,m);
+	cout<<((m*n)/result)<<endl;
+	return 0;
+}
+
+long long gcd(long long a, long long b){
+	if(b==0){
+		return a;
+	}
+	else{
+		return gcd(b, a%b);
+	}
+
+}
+
+
+
